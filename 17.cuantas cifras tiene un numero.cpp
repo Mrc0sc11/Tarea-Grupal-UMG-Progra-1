4fb367b8385cfc2d802e54f44b17eb2d62b26709
cr521 <cristhiancumayen@gmail.com>
2025-04-05 17:43:25 -0600

    Add files via upload

---
+++ 17.cuantas cifras tiene un numero.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main (void){
+	int  num;
+	cin>>num;
+	
+	int num_cifras = 1;
+	while(num>=10){
+		num=num/10;
+		num_cifras++;
+		
+	}
+	cout<<"cifras: "<<num_cifras;
+}
---
+++ 20.Conversio de minutos a segundos.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+	double horas;
+	double minutos;
+	double segundos;
+	cout<<"conversion de tiempo: "<<endl;
+	cout<<"Horas: ";
+	cin>>horas;
+	minutos = horas * 60;
+	segundos = horas*60*60;
+	cout<<"Minutos: "<<minutos<<endl;
+	cout<<"Segundos: "<<segundos<<endl;
+	
+	
+	
+	
+	return 0;
+	
+}
